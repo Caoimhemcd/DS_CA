f6f03fe5c043a67150ee95c0834da298eb0ae2be
Caoimhemcd <x20164599@student.ncirl.ie>
2021-12-12 17:54:02 +0000

    supplies server initial

--- src/main/java/grpc/smartOffice/SuppliesServer.java
+++ src/main/java/grpc/smartOffice/SuppliesServer.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import grpc.smartOffice.suppliesGrpc.suppliesImplBase;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
 import simpleJMDNS.SimpleServiceRegistration;
 
 public class SuppliesServer extends suppliesImplBase{
@@ -13,7 +14,7 @@ public class SuppliesServer extends suppliesImplBase{
 	private static final Logger logger = Logger.getLogger(PrinterServer.class.getName());
 	
 	public static void main(String [] args) {
-		PrinterServer printerUpdate = new PrinterServer();
+		SuppliesServer supplies = new SuppliesServer();
 		
 		int port = 50052;
 		String service_type = "_suppliesgrpc._tcp.local.";
@@ -23,7 +24,7 @@ public class SuppliesServer extends suppliesImplBase{
 		
 		try {
 			Server server = ServerBuilder.forPort(port)
-			    .addService(printerUpdate)
+			    .addService(supplies)
 			    //.addService(greeterserver1)
 			    .build()
 			    .start();
@@ -44,6 +45,33 @@ public class SuppliesServer extends suppliesImplBase{
 	    logger.info("Server started, listening on " + port);
 		
 	}
+	
+	@Override
+	public StreamObserver<containsOfficeSupplies> orderSupplies(StreamObserver<containsOrderConfirmation> responseObserver) {
+		//System.out.println("On server; inside streaming method");
+		
+		return new StreamObserver<containsOfficeSupplies>(){
 
+			@Override
+			public void onNext(containsOfficeSupplies value) {
+				System.out.println("On server; message received from client: " + value.getQuantity() + " x " +value.getSupplyId());	
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				
+			}
+
+			@Override
+			public void onCompleted() {
+				//Now build response c&p from unary method
+				//builder
+				containsOrderConfirmation.Builder confirmation = containsOrderConfirmation.newBuilder();
+				confirmation.setConfirmation("Server for your stream message");
+				responseObserver.onNext(confirmation.build());
+				responseObserver.onCompleted();
+			}};
+	}
+	
 }
 
